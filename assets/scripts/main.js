6fabe6bfd4262e7681d578557fa28c16e1a8f26e
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2022-11-08 16:58:21 -0800

    Add expose portion, mostly there, perhaps small bug

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -69,14 +69,51 @@ async function getRecipes() {
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
   /**************************/
+  let lStorage = localStorage.getItem('recipes');
+  if(lStorage !== null){
+    return JSON.parse(lStorage);
+  }
+
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let recipeArray = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  let recipeFetchPromise = new Promise(async (resolve, reject) => {
+    let promiseList = [];
+    //A4
+    for(let i = 0; i < RECIPE_URLS.length; i++){
+      try {
+        //A6
+        let recipeURL = await fetch(RECIPE_URLS[i]);
+        //A7
+        let jsonResponse = await recipeURL.json();
+        //A8
+        recipeArray.push(jsonResponse);
+        //A9
+        promiseList.push(recipeURL);
+        if(i === RECIPE_URLS.length-1){               //&& Promise.all(promiseList).state === 'resolved'
+          Promise.all(promiseList).finally(() => {
+            saveRecipesToStorage(recipeArray);
+            resolve(recipeArray);
+          });
+        }
+        
+
+      } catch (error) {
+        console.error(error);
+        reject(error);
+      }
+    }
+
+
+
+  });
+  return recipeFetchPromise;
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
